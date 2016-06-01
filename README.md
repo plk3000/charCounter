fd42ec4d5d3090735b33572faeee412562b3081a
Juan Diego Solis Smith <jsolis@searchtechnologies.com>
2016-06-01 08:26:51 -0600

    Working Script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # charCounter
+
+Small script to display a list of all characters present in a file with a count of how many times that char is in the file.
+
+#### To run
+`node app.js pathToFile [printAsText]`
+
+- **_[ ]_** indicate an optional parameter
+- If **_printAsText_** is present the result is listed as *char  count*, if not present the result is an array of JSON objects in the form:
+```json
+{
+  "char": "A",
+  "count": 654
+}
+```
---
+++ app.js
@@ -0,0 +1,52 @@
+(function() {
+  'use strict';
+
+  const readline = require('readline');
+  const fs = require('fs');
+  const fileName = process.argv[2];
+  const printText = process.argv[3] || false;
+  var chars = {};
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream(fileName)
+  });
+
+  function compare(a, b) {
+    if (a.char < b.char)
+      return -1;
+    else if (a.char > b.char)
+      return 1;
+    else
+      return 0;
+  }
+
+  rl.on('line', (line) => {
+    var lineChars = line.split('');
+    lineChars.forEach(function(val) {
+      if (chars[val]) {
+        chars[val]++;
+      } else {
+        chars[val] = 1;
+      }
+    });
+  });
+
+  rl.on('close', () => {
+    var charsArray = [];
+    for (var char in chars) {
+      var charObject = {
+        "char": encodeURI(char),
+        "count": chars[char]
+      };
+      charsArray.push(charObject);
+    }
+    charsArray.sort(compare);
+    if (!printText) {
+      console.log(JSON.stringify(charsArray, null, '  '));
+    } else {
+      charsArray.forEach(function(value) {
+        console.log(value.char + '\t' + value.count);
+      });
+    }
+  });
+}());
